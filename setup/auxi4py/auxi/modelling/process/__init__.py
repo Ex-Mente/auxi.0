d8dc780409c9aefa539a916153ae5463a09f661c
christoffkok <christoff.kok@ex-mente.co.za>
2016-01-04 12:33:29 +0200

    Added the setup folder.

---
+++ setup/auxi4py/auxi/modelling/process/__init__.py
@@ -0,0 +1,5 @@
+"""auxi.modelling.process Package
+
+The purpose of this package is to provide common classes and functions for the rest of the auxi.modelling.process namespace"""
+
+__version__ = "0.0.0"
