70e7e1b0e2bf52ab7ad1aa71f0248343a9f31883
christoffkok <christoff.kok@ex-mente.co.za>
2016-01-07 14:53:38 +0200

    Version 0.0.4, building as release, and not debug anymore. Setup contains licenses and platform information now as well.

--- setup/auxi4py/auxi/__init__.py
+++ setup/auxi4py/auxi/__init__.py
@@ -4,6 +4,6 @@ The purpose of this package is to make auxi's modeling and simulation framework
 available through a series of Python modules for easy access and use.
 
 Python version:  3.4.0
-auxi version: 0.0.3"""
+auxi version: 0.0.4"""
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
