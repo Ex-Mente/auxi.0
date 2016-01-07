8bbb414a9f35169fb97b79938b1f3690fdde8ee0
christoffkok <christoff.kok@ex-mente.co.za>
2016-01-07 13:17:35 +0200

    Removed documentation from the setup

--- setup/auxi4py/setup.py
+++ setup/auxi4py/setup.py
@@ -7,11 +7,11 @@ from distutils.core import setup
 
 # build the distribution
 setup(name="auxi",
-      version="0.0.1",
+      version="0.0.2",
       description="auxi for Python",
       package_dir={'auxi': 'auxi'},
       packages=["auxi", "auxi.core", "auxi.tools", "auxi.tools.chemistry"],
       package_data={'auxi.core': ['*.so*','*.so.1.54.0*'],
                     'auxi.tools.chemistry': ['*.so*','*.so.1.54.0*', r'data/*'],
-                    'auxi' : [r'*.txt', r'doc/*.pdf', r'doc/*', r'doc/_static/*', r'doc/_sources/*']}
+                    'auxi' : [r'*.txt']}
       )
