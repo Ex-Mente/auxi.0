3d17dc21cc6e7f55c2e61a646dac145e3e9059a3
christoffkok <christoff.kok@ex-mente.co.za>
2016-01-18 12:04:40 +0200

    Removed Stock from modelling. We do not need it now. Fixed small documentation issues.

--- setup/auxi4py/auxi/modelling/accounting/__init__.py
+++
@@ -1,5 +0,0 @@
-"""auxi.modelling.accounting Package
-
-The purpose of this package is to provide common classes and functions for the rest of the auxi.modelling.accounting namespace"""
-
-__version__ = "0.1.0"
--- setup/auxi4py/auxi/modelling/accounting/financial/__init__.py
+++
@@ -1,7 +0,0 @@
-"""auxi.modelling.accounting.financial Package
-
-The purpose of this package is to provide common classes and functions for the rest of the auxi.modelling.accounting.financial namespace"""
-
-__version__ = "0.1.0"
-
-from auxi.modelling.accounting.financial._financial import *
