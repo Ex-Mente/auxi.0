3e5790bcc7d0ab381f2b9adbb2685b5bfbaabdbc
christoffkok <christoff.kok@ex-mente.co.za>
2016-01-20 12:31:36 +0200

    Fixing naming and python usage on the business and financial modules

--- setup/auxi4py/auxi/modelling/stock/__init__.py
+++
@@ -1,5 +0,0 @@
-"""auxi.modelling.accounting.stock Package
-
-The purpose of this package is to provide common classes and functions for the rest of the auxi.modelling.accounting.stock namespace"""
-
-__version__ = "0.1.0"
