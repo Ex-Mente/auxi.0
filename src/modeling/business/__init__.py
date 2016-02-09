1317765ec0be2850756fe27548478f63aae01b75
christoffkok <christoff.kok@ex-mente.co.za>
2016-02-09 07:21:35 +0200

    Making changes to the business module

---
+++ src/modeling/business/__init__.py
@@ -0,0 +1,6 @@
+"""auxi.modeling.business Package
+
+The purpose of this package is to provide common classes and functions for the the auxi.modeling.business namespace"""
+
+
+__version__ = "0.2.0"
---
+++ src/modeling/financial/reporting/__init__.py
@@ -0,0 +1,6 @@
+"""auxi.modeling.financial.reports Package
+
+The purpose of this package is to provide common classes and functions for the the auxi.modeling.financial.reports namespace"""
+
+
+__version__ = "0.2.0"
