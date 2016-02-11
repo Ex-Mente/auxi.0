83fbe0b0fc47e2dddb8822464c53e2e4e0f4ebf8
christoffkok <christoff.kok@ex-mente.co.za>
2016-02-11 15:07:32 +0200

    New version

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,10 +1,10 @@
 sh create_distribution
 rm -r /usr/local/lib/python3.4/dist-packages/auxi
-rm /usr/local/lib/python3.4/dist-packages/auxi-0.2.0rc2.egg-info
+rm /usr/local/lib/python3.4/dist-packages/auxi-0.2.0rc3.egg-info
 cd dist
-rm -r auxi-0.2.0rc2
-tar -xf auxi-0.2.0rc2.tar.gz
-cd auxi-0.2.0rc2
+rm -r auxi-0.2.0rc3
+tar -xf auxi-0.2.0rc3.tar.gz
+cd auxi-0.2.0rc3
 python3 setup.py install
 
 
