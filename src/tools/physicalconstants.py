f04b2f9f8e8c801c4bb53b252fd13165c7cfaf9e
Johan Zietsman <johan.zietsman@ex-mente.co.za>
2016-07-09 09:43:45 +0200

    #37 fixed a character problem

--- src/tools/physicalconstants.py
+++ src/tools/physicalconstants.py
@@ -17,38 +17,37 @@ __status__ = 'Planning'
 # Universal Constants
 
 c = 299792458
-"""[m·s−1] speed of light in vacuum"""
+"""[m·s-1] speed of light in vacuum"""
 
-G = 6.67408E−11
-"""[m3.kg−1.s−2] Newtonian constant of gravitation"""
+G = 6.67408E-11
+"""[m3.kg-1.s-2] Newtonian constant of gravitation"""
 
-h = 6.626070040E−34
+h = 6.626070040E-34
 """[J.s] Planck constant"""
 
-h_bar = 1.054571800E−34
+h_bar = 1.054571800E-34
 """[J.s] reduced Planck constant (h/(2pi))"""
 
 
 # Physico-chemical Constants
 
-m_u = 1.660538921E−27
+m_u = 1.660538921E-27
 """[kg] atomic mass constant"""
 
 N_A = 6.02214129E23
-"""[mol−1] Avogadro's number"""
+"""[mol-1] Avogadro's number"""
 
-k_B = 1.3806488E−23
-"""[J.K−1] Boltzmann constant"""
+k_B = 1.3806488E-23
+"""[J.K-1] Boltzmann constant"""
 
 F = 96485.3365
-"""[C·mol−1] Faraday constant"""
+"""[C·mol-1] Faraday constant"""
 
 R = 8.3144621
-"""[J.K−1.mol−1] gas constant"""
+"""[J.K-1.mol-1] gas constant"""
 
 
 # Other Constants
 
 g = 9.80665
-"""[m.s−2] standard acceleration of gravity"""
-
+"""[m.s-2] standard acceleration of gravity"""
