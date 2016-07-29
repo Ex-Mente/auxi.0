1118054e7ed8084d368e732b889956cee856042f
christoffkok <christoff.kok@ex-mente.co.za>
2016-07-29 14:50:38 +0200

    Issue 142 (#146)
    * Update MAKING-RELEASES.md. This closes #96, #100
    
    * Added new release, 0.3.2's release notes.
    
    * Upgraded version to 0.3.2 (#145)

--- src/modelling/process/materials/__init__.py
+++ src/modelling/process/materials/__init__.py
@@ -5,7 +5,7 @@ materials and material packages.
 """
 
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __license__ = 'LGPL v3'
 __copyright__ = 'Copyright 2016, Ex Mente Technologies (Pty) Ltd'
 __author__ = 'Christoff Kok, Johan Zietsman'
