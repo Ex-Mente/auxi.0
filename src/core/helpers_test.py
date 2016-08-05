39ef7283ffde9c000bfc330ba856b8a89a3f761a
christoffkok <christoff.kok@ex-mente.co.za>
2016-08-05 09:48:46 +0200

    0.3.3 (#163)
    * New Release: updated version numbers to 0.3.3
    
    * Update RELEASE-NOTES.md for 0.3.3. This closes #162

--- src/core/helpers_test.py
+++ src/core/helpers_test.py
@@ -9,7 +9,7 @@ from datetime import datetime, date
 
 from auxi.core.helpers import get_path_relative_to_module, get_date
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __license__ = 'LGPL v3'
 __copyright__ = 'Copyright 2016, Ex Mente Technologies (Pty) Ltd'
 __author__ = 'Christoff Kok, Johan Zietsman'
