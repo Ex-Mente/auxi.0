aa3b1e4c3b9909ba0e22a81f3f5b20583a4f165c
Johan Zietsman <johan.zietsman@ex-mente.co.za>
2016-11-03 10:03:19 +0200

    I added some infrastructure for developing models. I also added a MaterialStream class to describe material rates. Having only a MaterialPackage class that describes material states is not sufficient.

---
+++ src/modelling/core_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module provides testing code for classes in the chem module.
+"""
+
+import unittest
+
+from auxi.modelling.core import Project
+
+
+__version__ = '0.3.3'
+__license__ = 'LGPL v3'
+__copyright__ = 'Copyright 2016, Ex Mente Technologies (Pty) Ltd'
+__author__ = 'Christoff Kok, Johan Zietsman'
+__credits__ = ['Christoff Kok', 'Johan Zietsman']
+__maintainer__ = 'Christoff Kok'
+__email__ = 'christoff.kok@ex-mente.co.za'
+__status__ = 'Planning'
+
+
+class ProjectTester(unittest.TestCase):
+    """
+    Tester for the auxi.modelling.core.Project class.
+    """
+
+    def test_constructor(self):
+        Model('Test Project', 'Test project.')
+
+
+if __name__ == '__main__':
+    unittest.main()
