7099c2b27890d8965a6d453e032ab37ebecae885
Johan Zietsman <johan.zietsman@ex-mente.co.za>
2016-11-19 21:20:38 +0200

    Remove enum34 dependency. This closes #184.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='auxi',
                              'examples/tools/materialphysicalproperties/data/readme',
                              'examples/tools/transportphenomena/heattransfer/*.ipynb'
                              ]},
-      install_requires=['jsonpickle', 'tabulate', 'enum34', 'python-dateutil',
+      install_requires=['jsonpickle', 'tabulate', 'python-dateutil',
                         'cerberus<=0.9.2', 'pandas', 'bibtexparser'],
       test_suite='auxi.tests'
       )
