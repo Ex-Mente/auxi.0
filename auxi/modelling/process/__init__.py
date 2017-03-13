3ce566e1fd573daf9b713457c9bbd1e019b0d692
Johan Zietsman <johan.zietsman@ex-mente.co.za>
2017-03-13 17:35:16 +0200

    Release 0.3.4. I also fixed the auxi thermochemistry file formats. The Rao and NIST data could not be loaded with Compound.read. Compound.read was also not used in load_data_auxi, which I fixed.

--- auxi/modelling/process/__init__.py
+++ auxi/modelling/process/__init__.py
@@ -5,7 +5,7 @@ for the the auxi.modelling.process namespace
 """
 
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __license__ = 'LGPL v3'
 __copyright__ = 'Copyright 2016, Ex Mente Technologies (Pty) Ltd'
 __author__ = 'Christoff Kok, Johan Zietsman'
