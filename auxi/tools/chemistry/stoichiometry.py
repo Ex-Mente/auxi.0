7f8bb754573bf088ad097da99a5d5f45c255f1fe
Johan Zietsman <johan.zietsman@ex-mente.co.za>
2017-04-01 15:17:44 +0200

    Correct docstring.
    I found a mistake in the mass_fractions function docstring, which I fixed.

--- auxi/tools/chemistry/stoichiometry.py
+++ auxi/tools/chemistry/stoichiometry.py
@@ -255,9 +255,9 @@ def masses(amounts):
 
 def mass_fractions(amounts):
     """
-    Calculate the mole fractions from the specified compound masses.
+    Calculate the mole fractions from the specified compound amounts.
 
-    :param masses: [kg] dictionary, e.g. {'SiO2': 3.0, 'FeO': 1.5}
+    :param amounts: [kmol] dictionary, e.g. {'SiO2': 3.0, 'FeO': 1.5}
 
     :returns: [mass fractions] dictionary
     """
